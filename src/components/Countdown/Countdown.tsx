ec6458fc13859ba1c22468f8d1408f960013bc8c
Julius <julius.plass@hotmail.com>
2025-04-18 15:07:18 +0200

    * Added basic Countdown component.

---
+++ src/components/Countdown/Countdown.tsx
@@ -0,0 +1,10 @@
+import { FC } from 'react';
+import './Countdown.scss';
+
+const Countdown: FC = props => (
+  <div className="Countdown">
+    [COUNTDOWN]
+  </div>
+);
+
+export default Countdown;
