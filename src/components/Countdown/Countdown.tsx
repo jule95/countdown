51e15db16674e36617e3adce883551b1478c1fbf
Julius <julius.plass@hotmail.com>
2025-04-20 11:35:25 +0200

    WIP

--- src/components/Countdown/Countdown.tsx
+++ src/components/Countdown/Countdown.tsx
@@ -1,9 +1,16 @@
 import { FC } from 'react';
 import './Countdown.scss';
+import Digit from '../Digit/Digit.tsx';
 
+// eslint-disable-next-line
 const Countdown: FC = props => (
   <div className="Countdown">
-    [COUNTDOWN]
+    <Digit
+      number={0}
+      size={10} />
+    <Digit
+      number={1}
+      size={10} />
   </div>
 );
 
---
+++ src/components/Point/Point.tsx
@@ -0,0 +1,11 @@
+import './Point.scss';
+import { FC } from 'react';
+
+// eslint-disable-next-line
+const Point:FC = ({}) => {
+  return (
+    <div className="Point">[POINT]</div>
+  );
+};
+
+export default Point;
