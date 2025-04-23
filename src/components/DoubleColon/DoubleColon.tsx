097ab902279600dfe7a1d6a2e1165d1e3735dbc3
Julius <julius.plass@hotmail.com>
2025-04-23 17:42:14 +0200

    * Basic Component

---
+++ src/components/DoubleColon/DoubleColon.tsx
@@ -0,0 +1,10 @@
+import './DoubleColon.scss';
+import { FC } from 'react';
+
+const DoubleColon: FC = () => {
+  return (
+    <div className="DoubleColon">[DOUBLECOLON]</div>
+  );
+};
+
+export default DoubleColon;
