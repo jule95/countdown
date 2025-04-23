b14bbdb3b060ddbe22665020d32b7a8685f449cc
Julius <julius.plass@hotmail.com>
2025-04-23 17:43:10 +0200

    * Commit Fix

--- src/components/DoubleColon/DoubleColon.tsx
+++ src/components/DoubleColon/DoubleColon.tsx
@@ -1,6 +1,7 @@
 import './DoubleColon.scss';
 import { FC } from 'react';
 
+// eslint-disable-next-line
 const DoubleColon: FC = () => {
   return (
     <div className="DoubleColon">[DOUBLECOLON]</div>
