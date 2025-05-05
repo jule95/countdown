b05ab0c89980b549276564bc5a0b8a454e2eafbb
Julius <julius.plass@hotmail.com>
2025-05-05 14:39:19 +0200

    * Added button to Navbar component to open modal.

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,15 +2,31 @@ import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Box from '@mui/material/Box';
 import LanguageSwitch from '../LanguageSwitch/LanguageSwitch.tsx';
+import AddBoxIcon from '@mui/icons-material/AddBox';
+import { ButtonBase } from '@mui/material';
+import { useContext } from 'react';
+import AppContext from '../../state/app-context.ts';
 
-const Navbar = () => (
-  <AppBar position="absolute">
-    <Toolbar variant="dense">
-      <Box sx={{ marginLeft: `auto` }}>
-        <LanguageSwitch />
-      </Box>
-    </Toolbar>
-  </AppBar>
-);
+const Navbar = () => {
+  const { actions } = useContext(AppContext);
+
+  return (
+    <AppBar position="absolute">
+      <Toolbar variant="dense">
+        <Box sx={{
+          display: `flex`,
+          marginLeft: `auto`,
+        }}>
+          <ButtonBase
+            disableRipple
+            onClick={actions.toggleModal}>
+            <AddBoxIcon sx={{ marginRight: `14px ` }} />
+          </ButtonBase>
+          <LanguageSwitch />
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
+};
 
 export default Navbar;
